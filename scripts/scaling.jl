3d68aad76acaa6cdd03696dc984e23a565084a63
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2019-08-30 18:44:44 +0300

    Fix some errors

--- scripts/scaling.jl
+++ scripts/scaling.jl
@@ -1,4 +1,5 @@
 using Plots
+using LaTeXStrings
 pgfplots()
 
 # function save(p, fn)
@@ -17,15 +18,15 @@ pgfplots()
 #     end
 # end
 
-P_A(n; p=0.9) = 1/(1-p + p/n)
-P_G(n; p=0.9) = 1 - p + n*p
+P_A(n; p=0.9965) = 1/(1-p + p/n)
+P_G(n; p=0.9965) = 1 - p + n*p
 
 ps = [0.5, 0.75, 0.9, 0.95]
 
 pa = plot([n->P_A(n, p=p) for p in ps], 1, 65535,
     label=[latexstring("p=$p") for p in ps],
-    xaxis=:log2, #yaxis=:log2,
-    xlabel="Numper of Processors", ylabel="Speedup",
+    xaxis=:log2,
+    xlabel="Number of Processors", ylabel="Speedup",
     legend=:topleft,
     minorgrid=true, minorgridalpha=0.1,
     framestyle=:box,
@@ -36,8 +37,8 @@ savefig(pa, "figures/theoretical-scaling-Amdahl.tex")
 
 pg = plot([n->P_G(n, p=p) for p in ps], 1, 65535,
     label=[latexstring("p=$p") for p in ps],
-    # xaxis=:log2, #yaxis=:log2,
-    xlabel="Numper of Processors", ylabel="Speedup",
+    xaxis=:log2,
+    xlabel="Number of Processors", ylabel="Speedup",
     legend=:topleft,
     minorgrid=true, minorgridalpha=0.1,
     framestyle=:box,
@@ -46,14 +47,26 @@ pg = plot([n->P_G(n, p=p) for p in ps], 1, 65535,
 
 savefig(pg, "figures/theoretical-scaling-Gustafson.tex")
 
-p = plot([P_A, P_G, identity], 1, 20,
+p1 = plot([P_A, P_G, identity], 1, 20,
     label=["strong scaling", "weak scaling", "perfect"],
     # xaxis=:log10, yaxis=:log10,
-    xlabel="Numper of Processors", ylabel="Speedup",
+    xlabel="Number of Processors", ylabel="Speedup",
     legend=:topleft,
     minorgrid=true, minorgridalpha=0.1,
     framestyle=:box,
     tex_output_standalone=true
 )
+
+p2 = plot([P_A, P_G], 1, 65536,
+    label=["strong scaling", "weak scaling"],
+    xaxis=:log10, yaxis=:log10,
+    xlabel="Number of Processors", ylabel="Speedup",
+    legend=:topleft,
+    minorgrid=true, minorgridalpha=0.1,
+    framestyle=:box,
+    tex_output_standalone=true
+)
+
+p = plot(p1, p2, tex_output_standalone=true)
 
 savefig(p, "figures/theoretical-scaling-all.tex")
